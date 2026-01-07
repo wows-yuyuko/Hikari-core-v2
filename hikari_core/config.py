7c81f5bd86c1867ef3508128a68559a530b15c37
benx1n <1119809439@qq.com>
2026-01-07 11:28:09 +0800

    bump version 1.2.4

--- hikari_core/config.py
+++ hikari_core/config.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from typing import Optional
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from loguru import logger
 from pydantic import BaseModel
-from apscheduler.schedulers.background import BackgroundScheduler
 
 _scheduler = BackgroundScheduler(timezone='Asia/Shanghai')
 # 启动时只执行一次
@@ -26,16 +26,16 @@ hikari_config = Config_Model()
 
 
 def set_hikari_config(  # noqa: PLR0913
-        proxy: Optional[str] = None,
-        http2: bool = True,
-        token: Optional[str] = '123456:111111111111',
-        auto_rendering: bool = True,
-        auto_image: bool = True,
-        use_broswer: Optional[str] = 'chromium',
-        game_path: Optional[str] = '',
-        yuyuko_url: Optional[str] = 'https://v3-api.wows.shinoaki.com',
-        yuyuko_type: Optional[str] = 'BOT',
-        local_test: bool = False,
+    proxy: Optional[str] = None,
+    http2: bool = True,
+    token: Optional[str] = '123456:111111111111',
+    auto_rendering: bool = True,
+    auto_image: bool = True,
+    use_broswer: Optional[str] = 'chromium',
+    game_path: Optional[str] = '',
+    yuyuko_url: Optional[str] = 'https://v3-api.wows.shinoaki.com',
+    yuyuko_type: Optional[str] = 'BOT',
+    local_test: bool = False,
 ):
     """配置Hikari-core
 
@@ -61,22 +61,29 @@ def set_hikari_config(  # noqa: PLR0913
     hikari_config.local_test = local_test
     # 为空则使用默认路径
     from hikari_core.cache_utils import get_cache_file, initial_cache_file
-    if game_path == '':
-        initial_cache_file(get_cache_file())
-    else:
-        initial_cache_file(Path(game_path))
+
+    try:
+        if game_path == '':
+            cache_dir = get_cache_file()
+            initial_cache_file(cache_dir)
+            logger.info(f'数据目录{cache_dir!s}')
+        else:
+            initial_cache_file(Path(game_path))
+    except Exception as e:
+        logger.error(f'初始化数据目录失败: {e}')
     logger.info(f'当前hikari-core配置\n{hikari_config}')
     global _initial_scheduler
     if _initial_scheduler:
-        logger.info("初始化任务....")
-        from hikari_core.game.help import update_template, update_ship_cache, update_ship_cache_cron
-        logger.info("执行初始模板更新...")
+        logger.info('初始化任务....')
+        from hikari_core.game.help import update_ship_cache, update_ship_cache_cron, update_template
+
+        logger.info('执行初始模板更新...')
         update_template()
-        logger.info("执行初始缓存更新...")
+        logger.info('执行初始缓存更新...')
         update_ship_cache()
-        logger.info(f"启动定时任务")
+        logger.info('启动定时任务')
         _scheduler.add_job(update_template, 'cron', hour='4,12')
         _scheduler.add_job(update_ship_cache_cron, 'cron', day_of_week='thu', hour=8, minute=0)
         _scheduler.start()
-        logger.info("初始化任务结束....")
+        logger.info('初始化任务结束....')
         _initial_scheduler = False
