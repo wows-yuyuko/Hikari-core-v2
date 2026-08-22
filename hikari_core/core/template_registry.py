80c786cfa08094653a37e52500ef263dab1a6b8e
JustOneSummer <myyuyukosama@gmail.com>
2026-08-22 16:10:20 +0800

    chore: 移除 TEXT 注册与 text.html 模板（帮助指令已改用 H5 页）

--- hikari_core/core/template_registry.py
+++ hikari_core/core/template_registry.py
@@ -66,7 +66,6 @@ class Templates:
     WWS_UNBAN: TemplateConfig = TemplateConfig("wws-unban.html", 900, 100)
     WWS_SX: TemplateConfig = TemplateConfig("wws-sx.html", 920, 1000)
     WWS_BOX_CHRISTMAS: TemplateConfig = TemplateConfig("wws-box-christmas.html", 920, 1000)
-    TEXT: TemplateConfig = TemplateConfig("text.html", 800, 10)
 
     # 帮助（中/英 H5 帮助页）
     HELP_ZH: TemplateConfig = TemplateConfig("help-zh.html", 900, 10)
