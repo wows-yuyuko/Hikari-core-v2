66066dddc6d12fa3fb9768318372010928740d40
JustOneSummer <myyuyukosama@gmail.com>
2026-09-12 08:59:39 +0800

    军团排名改为军团战最佳赛季

--- tests/test.py
+++ tests/test.py
@@ -29,7 +29,7 @@ async def start():
     # await command("asia nahida_official ship 哥伦布")
     # await command("ban cn 西行寺雨季")
     # await command("ships 10 日本")
-    await command("me")
+    # await command("me")
     await command("clan asia YU_RI")
     # await command("cn 西行寺雨季")
     # await command("近期 30")
